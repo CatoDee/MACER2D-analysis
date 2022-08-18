ad3579c7288e7037621f432089a8ed9e80c63c0c
狄逸焕 <yihuan.dee@outlook.com>
2022-08-18 15:46:26 +0800

    constant

---
+++ macer_constant.py
@@ -0,0 +1,19 @@
+k_b = 1.38e-16         #erg K^-1
+mp = 1.66e-24 #g
+gamma=5/3
+mui=0.64 #1 solar
+G= 112.31 #code unit
+
+#单位制
+M_sun = 2.5e7 #Msun
+#C TRANSLATES CODE UNITS INTO c.g.s
+#C SCALES: 1kpc; 1Gyr; 2.5*10^7Msun
+#C LU:length ; TU: time ; MU:  mass
+#DU:density; EU:energy; VU:volume
+LU=3.08568025e21
+TU=3.15360000e16
+MU=4.97230000e40
+EU = 4.76042745e50
+DU = 1.6924060e-24
+VU = 2.93800656e64
+
